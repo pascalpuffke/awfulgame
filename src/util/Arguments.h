7b41c7aab0c32274620dd72e1ac93b7e791aa21f
Pascal Puffke <pascal@pascalpuffke.de>
2022-03-19 22:01:28 +0100

    Replace int32_t with int
    Only use <cstdint> aliases for platform dependent or longer to write types (long long int => int64_t, unsigned int => uint32_t...)
    
    Signed-off-by: Pascal Puffke <pascal@pascalpuffke.de>

--- src/util/Arguments.h
+++ src/util/Arguments.h
@@ -8,24 +8,24 @@ namespace Arguments {
 struct Configuration {
     bool vsync {};
     bool debug {};
-    int32_t fps {};
+    int fps {};
     std::string resourcePath {}; // TODO
 };
 
-Configuration parseArguments(int argc, char **argv)
-{
+Configuration parseArguments(int argc, char **argv) {
     auto options = cxxopts::Options("game", "A simple 2D game, written in C++20 using the raylib library");
     options.add_options()
-        ("h,help", "Print help")
-        ("r,res", "Set resource folder", cxxopts::value<std::string>()->default_value("../res"))
-        ("f,fps", "Max FPS", cxxopts::value<int32_t>()->default_value(std::to_string(std::numeric_limits<int>::max())))
-        ("d,debug", "Start in debugging mode", cxxopts::value<bool>()->default_value("false"))
-        ("v,vsync", "Enable vertical synchronization", cxxopts::value<bool>()->default_value("false"));
+            ("h,help", "Print help")
+            ("r,res", "Set resource folder", cxxopts::value<std::string>()->default_value("../res"))
+            ("f,fps", "Max FPS",
+             cxxopts::value<int>()->default_value(std::to_string(std::numeric_limits<int>::max())))
+            ("d,debug", "Start in debugging mode", cxxopts::value<bool>()->default_value("false"))
+            ("v,vsync", "Enable vertical synchronization", cxxopts::value<bool>()->default_value("false"));
     options.allow_unrecognised_options();
 
     auto result = options.parse(argc, argv);
 
-    for (auto &arg : result.unmatched()) {
+    for (auto &arg: result.unmatched()) {
         fmt::print(fg(fmt::color::yellow), "WARN: Unrecognised argument '{}'\n", arg);
     }
 
@@ -35,10 +35,10 @@ Configuration parseArguments(int argc, char **argv)
     }
 
     return Configuration {
-        .vsync = result["vsync"].as<bool>(),
-        .debug = result["debug"].as<bool>(),
-        .fps = result["fps"].as<int32_t>(),
-        .resourcePath = result["res"].as<std::string>()
+            .vsync = result["vsync"].as<bool>(),
+            .debug = result["debug"].as<bool>(),
+            .fps = result["fps"].as<int>(),
+            .resourcePath = result["res"].as<std::string>()
     };
 }
 
