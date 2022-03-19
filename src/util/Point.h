126fbe1b0def397d05a13fded7ad60e1cf782502
Pascal Puffke <pascal@pascalpuffke.de>
2022-03-19 21:51:36 +0100

    Template a more general Point struct
    and alias the previously used IntPoint as a Point<int>. This brings several benefits, as it is now easier to add e.g. a FloatPoint without having to rewrite 16 functions (though for that specifically, I'd rather use a raylib::Vector2).
    
    We also get rid of the <cstdint> include, no idea why I wanted to use int32_t instead of plain int.
    
    Signed-off-by: Pascal Puffke <pascal@pascalpuffke.de>

--- src/util/Point.h
+++ src/util/Point.h
@@ -1,16 +1,73 @@
 #pragma once
 
-#include <cstdint>
+template<typename T>
+struct Point {
+    T x;
+    T y;
 
-/// @brief A point in 2D space with signed 32-bit integer coordinates.
-struct IntPoint {
-    int32_t x;
-    int32_t y;
+    Point() = default;
 
-    [[nodiscard]] constexpr bool inBounds(IntPoint size, bool positive = true) const
-    {
+    Point(T x, T y) : x(x), y(y) {}
+
+    Point(const Point &) = default;
+    Point(Point &&) noexcept = default;
+    Point &operator=(const Point &) = default;
+    Point &operator=(Point &&) noexcept = default;
+
+    bool operator==(const Point &other) const {
+        return x == other.x && y == other.y;
+    }
+
+    bool operator!=(const Point &other) const {
+        return *this != other;
+    }
+
+    Point operator+(const Point &other) const {
+        return { x + other.x, y + other.y };
+    }
+
+    Point operator-(const Point &other) const {
+        return { x - other.x, y - other.y };
+    }
+
+    Point operator*(T scalar) const {
+        return { x * scalar, y * scalar };
+    }
+
+    Point operator/(T scalar) const {
+        return { x / scalar, y / scalar };
+    }
+
+    Point &operator+=(const Point &other) {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
+
+    Point &operator-=(const Point &other) {
+        x -= other.x;
+        y -= other.y;
+        return *this;
+    }
+
+    Point &operator*=(T scalar) {
+        x *= scalar;
+        y *= scalar;
+        return *this;
+    }
+
+    Point &operator/=(T scalar) {
+        x /= scalar;
+        y /= scalar;
+        return *this;
+    }
+
+    [[nodiscard]] constexpr bool inBounds(Point size, bool positive = true) const {
         if (positive)
             return x >= 0 && y >= 0 && x < size.x && y < size.y;
         return x < size.x && y < size.y;
     }
 };
+
+/// @brief A point in 2D space with signed 32-bit integer coordinates.
+using IntPoint = Point<int>;
